b40f3b6cad588412cebdad89afb1dbeb21e7cf7c
Gonzalonoe <gonza2002noe@gmail.com>
2024-05-15 13:50:43 -0300

    nuevo carrousel

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
 
-/* CARROUSEL */
+/* CARROUSEL 1*/
 const carrousel_container_img = document.querySelector(".carrousel_container_img")
 const flecha = document.querySelectorAll (".flecha")
 
@@ -10,3 +10,43 @@ flecha.forEach( ( cadaFlecha , i )=> {
         
     })
 })
+
+/*CARROUSEL 2*/
+
+var imagenes = [
+    "imagenesevento/imagen-1.jpg",
+    "imagenesevento/imagen-2.jpg",
+    "imagenesevento/imagen-3.jpg",
+    "imagenesevento/imagen-4.jpg",
+    "imagenesevento/imagen-5.jpg",
+    "imagenesevento/imagen-6.jpg",
+    "imagenesevento/imagen-7.jpg",
+    "imagenesevento/imagen-8.jpg",
+    "imagenesevento/imagen-9.jpg"
+];
+
+document.imagen.src = imagenes[0];
+
+var flechaderecha = document.querySelector(".flecha_derecha");
+var flechaizquierda = document.querySelector(".flecha_izquierda");
+var contador = 0;
+
+function derecha(){
+    contador++;
+    if (contador > imagenes.length - 1){
+        contador = 0;
+    }
+    document.imagen.src = imagenes[contador];
+}
+
+flechaderecha.addEventListener("click", derecha)
+
+function izquierda(){
+    contador--;
+    if (contador < 0){
+        contador = imagenes.length - 1;
+    }
+    document.imagen.src = imagenes[contador];
+}
+
+flechaizquierda.addEventListener("click", izquierda);
